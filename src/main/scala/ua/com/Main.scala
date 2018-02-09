229668ee1051d33ab49840a0d376bda4ebfa6ef1
Maria Rabyk <mariia.rabyk@gmail.com>
2018-02-09 18:44:03 +0200

    Update main, add end points

--- src/main/scala/ua/com/Main.scala
+++ src/main/scala/ua/com/Main.scala
@@ -1,40 +1,33 @@
 import akka.actor._
-import akka.http.scaladsl.server.Route
-import ua.com.actors._
-import ua.com.entity.InputURL
-import ua.com.routing.URLRoutes
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
+import ua.com.actors._
+import ua.com.routing.URLRoutes
 
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
 import scala.io.StdIn
+
 object Main extends URLRoutes{
 
+  implicit def system: ActorSystem = ActorSystem("URLShortener-akka")
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val executionContext = system.dispatcher
+ /* val validator: ActorRef = system.actorOf(ValidateURLActor.props, "ValidatorActor")
+  val urlShortner: ActorRef = system.actorOf(MakeShortURLActor.props, "URLShortnerActor")
+  val registrator: ActorRef = system.actorOf(URLRegistryActor.props(urlShortner), "RegistratorActor")
+  val coordinator: ActorRef = system.actorOf(CoordinatorActor.props(validator, registrator, urlShortner), "CoordinatorActor")*/
+
+  implicit val registrator2: ActorRef = system.actorOf(URLRegistryActor2.props, "Registrator")
   def main(args: Array[String]): Unit = {
-    // Create the 'URLShortener' actor system
-    val system = ActorSystem("URLShortener-akka")
-    /* implicit val materializer: ActorMaterializer = ActorMaterializer()*/
+  /*  if (args.length == 0) println("No URL, please provide a valid URL.")
+    else coordinator ! InputURL(args(0))*/
+  lazy val routes: Route = url2Routes
 
-    val validator: ActorRef = system.actorOf(ValidateURLActor.props, "ValidatorActor")
-    val urlShortner: ActorRef = system.actorOf(MakeShortURLActor.props, "URLShortnerActor")
-    val registrator: ActorRef = system.actorOf(URLRegistryActor.props(urlShortner), "RegistratorActor")
-
-    val coordinator: ActorRef = system.actorOf(CoordinatorActor.props(validator, registrator, urlShortner), "CoordinatorActor")
-
-    if (args.length == 0) println("No URL, please provide a valid URL.")
-    else coordinator ! InputURL(args(0))
-
-
-    lazy val routes: Route = urlRoutes
-
-    val bindingFuture = Http().bindAndHandle(routes, "localhost", 8080)
-
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate())
-
-
-  }
+  val bindingFuture = Http().bindAndHandle(url2Routes, "localhost", 8080)
+  println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+  StdIn.readLine() // let it run until user presses return
+  bindingFuture
+    .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+    }
 }
