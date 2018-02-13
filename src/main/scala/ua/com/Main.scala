88a8e53e9c6b5191e7cc519c0778ec06538fb991
Maria Rabyk <mariia.rabyk@gmail.com>
2018-02-13 18:19:35 +0200

    Update files

--- src/main/scala/ua/com/Main.scala
+++ src/main/scala/ua/com/Main.scala
@@ -1,35 +1,58 @@
-import ua.com.URLService.UrlShortenerServiceImpl
+import java.sql.Connection
+
 import akka.actor._
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
+import ua.com.Service.UrlService
 import ua.com.actors._
 import ua.com.routing.URLRoutes
 
 import scala.io.StdIn
 
 object Main extends URLRoutes{
+  import ua.com.DBConfig._
 
   implicit def system: ActorSystem = ActorSystem("URLShortener-akka")
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
-  lazy val urlService = new UrlShortenerServiceImpl()
-  /* val validator: ActorRef = system.actorOf(ValidateURLActor.props, "ValidatorActor")
-  val urlShortner: ActorRef = system.actorOf(MakeShortURLActor.props, "URLShortnerActor")
-  val registrator: ActorRef = system.actorOf(URLRegistryActor.props(urlShortner), "RegistratorActor")
-  val coordinator: ActorRef = system.actorOf(CoordinatorActor.props(validator, registrator, urlShortner), "CoordinatorActor")*/
+  val conn = dbConnection
+  val urlService = new UrlService()
+  val registrator2: ActorRef = system.actorOf(URLRegistryActor2.props(urlService), "Registrator")
 
-  implicit val registrator2: ActorRef = system.actorOf(URLRegistryActor2.props(urlService), "Registrator")
+  private def createDbStructure(conn: Connection): Unit = {
+   // val sql2: String = "create INDEX ind_short on urls(short_url);"
+    val stmt = conn.createStatement()
+    try {
+      //drop schema urlShortener2
+      //create schema if not exists urlShortener2;
+      //set schema urlShortener2;
+      val sql1 = """
+        create table if not exists urls (
+        id int auto_increment primary key,
+        short_url varchar(255) not null,
+        long_url varchar(255) not null,
+        clicks int not null);"""
+      stmt.execute(sql1)
+    //  stmt.execute(sql2)
+      println("DB created")
+    } finally {
+      stmt.close
+    }
+  }
+
   def main(args: Array[String]): Unit = {
-    /*  if (args.length == 0) println("No URL, please provide a valid URL.")
-    else coordinator ! InputURL(args(0))*/
-    lazy val routes: Route = url2Routes
+    try {
+      lazy val routes: Route = url2Routes
+      val bindingFuture = Http().bindAndHandle(routes, "localhost", 8080)
 
-    val bindingFuture = Http().bindAndHandle(url2Routes, "localhost", 8080)
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
+      println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+      StdIn.readLine() // let it run until user presses return
+      bindingFuture
+        .flatMap(_.unbind()) // trigger unbinding from the port
+        .onComplete(_ => system.terminate()) // and shutdown when done
+    } finally {
+      conn.close()
+    }
   }
 }
--- src/main/scala/ua/com/actors/URLRegistryActor2.scala
+++ src/main/scala/ua/com/actors/URLRegistryActor2.scala
@@ -1,70 +1,28 @@
 package ua.com.actors
 
-import ua.com.URLService.UrlShortenerServiceImpl
 import akka.actor._
+import ua.com.Service.UrlService
 import ua.com.entity.{InputURL, ShortURL, ValidURL}
 
 object URLRegistryActor2 {
+case object AllStatsRequest
+  case class URLStatsRequest(url: String)
 
-  /* private def findKeyByValue(map: collection.mutable.Map[String, String], value: String): String = {
-    map.filter{ case (k, v) => v == value }.keys.mkString}
-
-    val urlPattern = "([A-Za-z]{3,9}:(?:\\/\\/)?)+([a-zA-Z0-9.-]+(:[a-zA-Z0-9.&%$-]+)*@)*" +
-      "((?!(10\\.|172\\.(1[6-9]|2\\d|3[01])\\.|192\\.168\\.).*)(?!255\\.255\\.255\\.255)" +
-      "(25[0-5]|2[1,3]\\d|[1]\\d\\d|[1-9]\\d|[1-9])(\\.(25[0-5]|2[0-4]\\d|[1]\\d\\d|[1-9]\\d|\\d)){3}|" +
-      "[^\\.\\-\\s]([a-zA-Z0-9-]+\\.)*[a-zA-Z0-9-]+\\." +
-      "(com|edu|gov|int|mil|net|org|biz|arpa|info|name|pro|aero|coop|museum|[a-zA-Z]{1,9})|" +
-      "(\\[(([\\da-fA-F]{4}(:|::)){1}([\\da-fA-F]{1,4}(:|::)){1,6}([:]{1,2})?" +
-      "([\\da-fA-F]{1,4}){1})\\])|((?:\\+?(\\d{1}))?[- (](\\d{3})[- )](\\d{3})[- ](\\d{1,4})))(:[0-9]+)*" +
-      "(\\/($|[a-zA-Z0-9\\.,\\?'\\\\\\+&%$#=~_\\-\\(\\)]+)?)*"
-
-    def isValid(url: String): Boolean = Pattern.matches(urlPattern, url)
-  val urlMap = collection.mutable.Map[String, String]()
-    val alphabet: String = "bcdfghjkmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ23456789-_"
-    val randomStringLength = 8
-
-    private def randomAlphaNumericString(chars: String): String = {
-      val sb = new StringBuilder
-      for (i <- 1 to randomStringLength) {
-        val randomNum = util.Random.nextInt(chars.length)
-        sb.append(chars(randomNum))
-      }
-      sb.toString
-    }*/
-
-  def props(urlService: UrlShortenerServiceImpl): Props = Props(new URLRegistryActor2(urlService))
   // def props: Props = Props[URLRegistryActor2]
+  def props(urlService: UrlService): Props = Props(new URLRegistryActor2(urlService))
 }
 
-class URLRegistryActor2(urlService: UrlShortenerServiceImpl) extends Actor {
-
+class URLRegistryActor2(urlService: UrlService) extends Actor {
+import URLRegistryActor2._
   def receive: Receive = {
-
-    /* case a: InputURL => {
-      if (isValid(a.url)) {
-        if (urlMap.exists(_._2 == a.url)) {
-          val short = findKeyByValue(urlMap, a.url)
-          sender() ! ShortURL(short)
-        }else {
-          val shortName = "www."+ randomAlphaNumericString(alphabet)
-          urlMap += shortName -> a.url
-          println(urlMap.toString)
-          sender() ! ShortURL(shortName)
-        }
-      }
-    }
-    case b: ShortURL =>
-      val inputURL = urlMap(b.url)
-      println("input: "+ inputURL)
-sender() ! InputURL(inputURL)
-  }*/
     case a: InputURL => {
       if (urlService.validate(a.url)) {
-        sender() ! urlService.shorten(ValidURL(a.url))
+        sender() ! urlService.fetchByLong(ValidURL(a.url))
       }
     }
     case b: ShortURL =>
-      sender() ! urlService.get(b.url)
-  }
-
+      sender() ! urlService.fetchByShort(b.url)
+    case AllStatsRequest => sender() ! urlService.getAllStats
+    case c: URLStatsRequest => sender() ! urlService.getURLStats(ShortURL(c.url))
+      }
 }
